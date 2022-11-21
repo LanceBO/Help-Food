50de3964ceb1a4b313d00010aca0962af01a6d81
ugo lonobile <lonodert@hotmail.fr>
2022-11-21 17:49:07 +0100

    booking controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,33 @@
 class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(params[:booking])
+  end
+
+  def destroy
+    @booking = Booking.find(params[:booking])
+  end
+
+ def validate
+ end
+
+def show
+  @booking = Booking.find(params[:id])
+
+end
+
+def index
+  @bookings = Booking.all
+end
+
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:user_id)
+  end
+
 end
