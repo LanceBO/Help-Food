b78c461ec9c7728f45a80f72b102620b23475698
Olivier Bouly <oliv.bouly@gmail.com>
2022-11-22 18:53:08 +0100

    debug

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,6 @@
 class Food < ApplicationRecord
   belongs_to :user
   has_one :booking, dependent: :destroy
-  validates :name, :expiration_date, :photo, presence: :true
+  validates :name, :expiration_date, presence: :true
   validates :category, presence: :true
 end
