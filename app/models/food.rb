865521ec9b6a972825aac7b8f0dac4327d1533b6
ugo lonobile <lonodert@hotmail.fr>
2022-11-23 16:01:58 +0100

    Dashboard Finish

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,6 @@
 class Food < ApplicationRecord
   belongs_to :user
-  has_one :booking, dependent: :destroy
+  has_many :bookings, dependent: :destroy
   validates :name, :expiration_date, presence: true
   validates :category, presence: true
 end
